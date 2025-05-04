54a40ae0b6994f6078a25539f6135392766be260
gert <gert.van.hamme@hotmail.com>
2025-05-04 10:15:43 +0200

    Setup

---
+++ ArcsomAssetManagement.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ArcsomAssetManagement.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArcsomAssetManagement.Api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+    public DbSet<Product> Products { get; set; }
+}
