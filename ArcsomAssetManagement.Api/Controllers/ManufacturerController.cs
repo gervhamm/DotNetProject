94ed25ad3a6ae8fc05e9c24b74fde9d4b009d78b
gert <gert.van.hamme@hotmail.com>
2025-05-04 11:00:54 +0200

    Added Controllers: Manufacturer and Product

---
+++ ArcsomAssetManagement.Api/Controllers/ManufacturerController.cs
@@ -0,0 +1,117 @@
+﻿using ArcsomAssetManagement.Api.Data;
+using ArcsomAssetManagement.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArcsomAssetManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ManufacturerController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger<ManufacturerController> _logger;
+
+    public ManufacturerController(ApplicationDbContext context, ILogger<ManufacturerController> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var manufacturers = await _context.Manufacturers.AsNoTracking().ToListAsync(stoppingToken);
+        if (manufacturers == null)
+        {
+            return NotFound("Not Found");
+        }
+        return Ok(manufacturers);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get([FromRoute] ulong id)
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var manufacturer = await _context.Manufacturers.AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == id, stoppingToken);
+        if (manufacturer == null)
+        {
+            return NotFound("Not Found");
+        }
+        return Ok(manufacturer);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] Manufacturer request)
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var manufacturer = new Manufacturer
+        {
+            Name = request.Name,
+            Contact = request.Contact,
+            Products = request.Products
+        };
+
+        await _context.Manufacturers.AddAsync(manufacturer, stoppingToken);
+        await _context.SaveChangesAsync(stoppingToken);
+
+        return Ok(manufacturer);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> Update([FromRoute] ulong id, [FromBody] Manufacturer request)
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var manufacturer = await _context.Manufacturers.FirstOrDefaultAsync(c => c.Id == id, stoppingToken);
+        if (manufacturer == null)
+        {
+            return NotFound("Not Found");
+        }
+        manufacturer.Name = request.Name;
+        manufacturer.Contact = request.Contact;
+        manufacturer.Products = request.Products;
+
+        await _context.SaveChangesAsync(stoppingToken);
+
+        return Ok(manufacturer);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Remove(ulong id)
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var manufacturer = await _context.Manufacturers.Include(p => p.Products).FirstOrDefaultAsync(c => c.Id == id, stoppingToken);
+
+        if (manufacturer == null)
+        {
+            return NotFound("Not Found");
+        }
+
+        if (manufacturer.Products != null)
+        {
+            _context.Products.RemoveRange(manufacturer.Products);
+        }
+
+        _context.Manufacturers.Remove(manufacturer);
+        await _context.SaveChangesAsync(stoppingToken);
+
+        return NoContent();
+    }
+}
+
---
+++ ArcsomAssetManagement.Api/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using ArcsomAssetManagement.Api.Data;
+using ArcsomAssetManagement.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArcsomAssetManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(ApplicationDbContext context, ILogger<ProductController> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var products = await _context.Products.AsNoTracking().ToListAsync(stoppingToken);
+        if (products == null)
+        {
+            return NotFound("Not Found");
+        }
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get([FromRoute] ulong id)
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var product = await _context.Products.AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == id, stoppingToken);
+        if (product == null)
+        {
+            return NotFound("Not Found");
+        }
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] Product request)
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var product = new Product
+        {
+            Name = request.Name,
+            Manufacturer = request.Manufacturer
+        };
+
+        await _context.Products.AddAsync(product, stoppingToken);
+        await _context.SaveChangesAsync(stoppingToken);
+
+        return Ok(product);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> Update([FromRoute] ulong id, [FromBody] Product request)
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var product = await _context.Products.FirstOrDefaultAsync(c => c.Id == id, stoppingToken);
+        if (product == null)
+        {
+            return NotFound("Not Found");
+        }
+        product.Name = request.Name;
+        product.Manufacturer = request.Manufacturer;
+        await _context.SaveChangesAsync(stoppingToken);
+
+        return Ok(product);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Remove(ulong id)
+    {
+        var source = new CancellationTokenSource();
+        source.CancelAfter(TimeSpan.FromSeconds(10));
+        var stoppingToken = source.Token;
+
+        var product = await _context.Products.FirstOrDefaultAsync(c => c.Id == id, stoppingToken);
+
+        if (product == null)
+        {
+            return NotFound("Not Found");
+        }
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync(stoppingToken);
+
+        return NoContent();
+
+    }
+}
